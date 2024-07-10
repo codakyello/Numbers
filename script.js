5c7fd5c0b1f8ac1f1a61cd3ae80a016e56186a86
thePythoninja <olaoluwathompson8@gmail.com>
2024-07-10 21:01:04 +0100

    upgrades

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // I need it to pick and display a random number starting with 10 iterations
+
 let url =
-  "https://api.sheety.co/ebea7191455d4cbd3ebd1ab58d71433d/octaFx/111 &12+Tickets";
+  "https://api.sheety.co/ebea7191455d4cbd3ebd1ab58d71433d/octaFxV2/2NdLevel3+tickets";
 
 async function getTicketNumber() {
   const res = await fetch(url);
   const data = await res.json();
-  const tickets = data["111 &12+Tickets"];
+  const tickets = data["2NdLevel3+tickets"];
+
+  console.log(tickets);
 
   return tickets.map((ticket) => ticket.ticketNumber);
 }
@@ -24,12 +27,20 @@ let ticketNumbers = "";
   ticketNumbers = await getTicketNumber();
 })();
 
+let reset = false;
+const duration = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 async function Lottery() {
-  for (let i = 0; i < 30; i++) {
+  const durationLength = Math.floor(Math.random() * duration.length) + 1;
+
+  for (let i = 0; i < duration[durationLength]; i++) {
+    console.log(duration[durationLength]);
     // Generate a random number from 0 to length of arr
     const randNum = Math.floor(Math.random() * ticketNumbers.length);
     // Add a one second delay
     // Set interval is an
+
+    console.log(reset);
+    if (reset) return;
 
     await wait(0.1);
     if (ticketNumbers) {
@@ -40,8 +51,12 @@ async function Lottery() {
   }
 }
 
-btnEl.addEventListener("click", Lottery);
+btnEl.addEventListener("click", () => {
+  reset = false;
+  Lottery();
+});
 
 resetEl.addEventListener("click", () => {
-  numEl.innerHTML = "💰";
+  reset = true;
+  numEl.innerHTML = "_";
 });
