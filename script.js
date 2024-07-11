e7986364ace3694260aa9fd3625e911b47a092da
thePythoninja <olaoluwathompson8@gmail.com>
2024-07-11 09:10:52 +0100

    upgrades

--- script.js
+++ script.js
@@ -28,19 +28,26 @@ let ticketNumbers = "";
 })();
 
 let reset = false;
-const duration = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let isSpinning = false;
+// const duration = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 async function Lottery() {
-  const durationLength = Math.floor(Math.random() * duration.length) + 1;
+  // const durationLength = Math.floor(Math.random() * duration.length) + 1;
 
-  for (let i = 0; i < duration[durationLength]; i++) {
-    console.log(duration[durationLength]);
+  isSpinning = true;
+  console.log(isSpinning);
+  for (let i = 0; i < 55; i++) {
     // Generate a random number from 0 to length of arr
     const randNum = Math.floor(Math.random() * ticketNumbers.length);
     // Add a one second delay
     // Set interval is an
 
-    console.log(reset);
-    if (reset) return;
+    if (reset) {
+      numEl.innerHTML = "_";
+      isSpinning = false;
+      drumRoll.pause(); // Pause the audio
+      drumRoll.currentTime = 0;
+      return;
+    }
 
     await wait(0.1);
     if (ticketNumbers) {
@@ -49,14 +56,29 @@ async function Lottery() {
 
     //   console.log(randNum[arr]);
   }
+  isSpinning = false;
 }
 
-btnEl.addEventListener("click", () => {
+// once i click pick a winner spinning should be set to true and
+// when true we shouldnt be able to go for a second spin until after the spinning is completed.
+
+const jsConfetti = new JSConfetti();
+var drumRoll = new Audio("./drum-roll-sound-effect.mp3");
+var crash = new Audio("./crash.mp3");
+btnEl.addEventListener("click", async () => {
   reset = false;
-  Lottery();
+  if (!isSpinning) {
+    drumRoll.play();
+    await Lottery();
+    if (!reset) {
+      crash.play();
+      jsConfetti.addConfetti();
+    }
+  }
 });
 
 resetEl.addEventListener("click", () => {
+  // jsConfetti.addConfetti();
+
   reset = true;
-  numEl.innerHTML = "_";
 });
